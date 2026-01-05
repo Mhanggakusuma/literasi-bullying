91c02231b6ad022f7463b791972ba7d168d2e4fc
Mhanggakusuma <l200220133@student.ums.ac.id>
2026-01-05 13:13:58 +0700

    safe auto create admin command

--- users/management/commands/create_admin.py
+++ users/management/commands/create_admin.py
@@ -1,18 +1,29 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
 
 class Command(BaseCommand):
-    help = "Create admin user"
+    help = "Create default admin if not exists"
 
     def handle(self, *args, **options):
-        User = get_user_model()
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "")
 
-        if not User.objects.filter(username="adminsmp1md3").exists():
-            User.objects.create_superuser(
-                username="adminsmp1md3",
-                email="adminsmp1md3@gmail.com",
-                password="Smp1md3333"
-            )
-            self.stdout.write("Admin created")
-        else:
-            self.stdout.write("Admin already exists")
+        if not username or not password:
+            self.stdout.write("Admin env not set, skipping.")
+            return
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write("Admin already exists.")
+            return
+
+        User.objects.create_superuser(
+            username=username,
+            password=password,
+            email=email
+        )
+
+        self.stdout.write("Admin user created successfully.")
