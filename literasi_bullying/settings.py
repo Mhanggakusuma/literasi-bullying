8dcac9a631fa0daaf993448843e3357b6689340b
Mhanggakusuma <l200220133@student.ums.ac.id>
2026-01-11 14:48:45 +0700

    fix(config): make database ssl conditional for local and production

--- literasi_bullying/settings.py
+++ literasi_bullying/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
     "default": dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
-        ssl_require=True,
+        ssl_require=os.environ.get("DATABASE_SSL", "false") == "true",
     )
 }
 
